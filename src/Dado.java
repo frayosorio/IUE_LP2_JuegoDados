b03b9a615f247cf0e136dbcd392dc02b4d20c812
fray osorio <frayosorio@gmail.com>
2025-02-24 11:18:57 -0500

    Se crea la clase DADO

---
+++ src/Dado.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Dado {
+
+    private int numero;
+
+    public void lanzar(Random r) {
+        // asignarle al numero del dado un valor al azar entr 1 y 6
+        numero = r.nextInt(6) + 1;
+    }
+
+    public void mostrar(JLabel lbl) {
+        String nombreArchivo = "/imagenes/" + numero + ".jpg";
+        ImageIcon imgDado = new ImageIcon(getClass().getResource(nombreArchivo));
+
+        lbl.setIcon(imgDado);
+    }
+
+}
--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,5 +1,8 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -9,6 +12,11 @@ import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 public class FrmJuego extends JFrame {
+
+    Dado dado1, dado2;
+    Random r = new Random();
+
+    JLabel lblDado1, lblDado2;
 
     // metodo constructor
     public FrmJuego() {
@@ -21,12 +29,12 @@ public class FrmJuego extends JFrame {
         String nombreArchivo = "/imagenes/1.jpg";
         ImageIcon imgDado = new ImageIcon(getClass().getResource(nombreArchivo));
 
-        JLabel lblDado1 = new JLabel();
+        lblDado1 = new JLabel();
         lblDado1.setIcon(imgDado);
         lblDado1.setBounds(10, 10, imgDado.getIconWidth(), imgDado.getIconHeight());
         getContentPane().add(lblDado1);
 
-        JLabel lblDado2 = new JLabel();
+        lblDado2 = new JLabel();
         lblDado2.setIcon(imgDado);
         lblDado2.setBounds(10 + imgDado.getIconWidth(), 10, imgDado.getIconWidth(), imgDado.getIconHeight());
         getContentPane().add(lblDado2);
@@ -43,7 +51,7 @@ public class FrmJuego extends JFrame {
 
         JLabel lblLanzamientos = new JLabel("0");
         lblLanzamientos.setBounds(30 + 2 * imgDado.getIconWidth(), 40, 100, 100);
-        lblLanzamientos.setBackground(new Color(0,0,0));
+        lblLanzamientos.setBackground(new Color(0, 0, 0));
         lblLanzamientos.setForeground(new Color(51, 255, 0));
         lblLanzamientos.setOpaque(true);
         lblLanzamientos.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -52,19 +60,45 @@ public class FrmJuego extends JFrame {
 
         JLabel lblCenas = new JLabel("0");
         lblCenas.setBounds(160 + 2 * imgDado.getIconWidth(), 40, 100, 100);
-        lblCenas.setBackground(new Color(0,0,0));
+        lblCenas.setBackground(new Color(0, 0, 0));
         lblCenas.setForeground(new Color(51, 255, 0));
         lblCenas.setOpaque(true);
         lblCenas.setHorizontalAlignment(SwingConstants.RIGHT);
         lblCenas.setFont(new Font("Tahoma", 1, 72));
         getContentPane().add(lblCenas);
 
-        JButton btnIniciar=new JButton("Iniciar");
-        btnIniciar.setBounds(10, 20+imgDado.getIconHeight(), 100, 25);
+        JButton btnIniciar = new JButton("Iniciar");
+        btnIniciar.setBounds(10, 20 + imgDado.getIconHeight(), 100, 25);
         getContentPane().add(btnIniciar);
 
-        JButton btnLanzar=new JButton("Lanzar");
-        btnLanzar.setBounds(10, 60+imgDado.getIconHeight(), 100, 25);
+        JButton btnLanzar = new JButton("Lanzar");
+        btnLanzar.setBounds(10, 60 + imgDado.getIconHeight(), 100, 25);
         getContentPane().add(btnLanzar);
+
+        btnIniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciar();
+            }
+        });
+
+        btnLanzar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lanzar();
+            }
+        });
+
+        dado1 = new Dado();
+        dado2 = new Dado();
+    }
+
+    private void iniciar() {
+
+    }
+
+    private void lanzar() {
+        dado1.lanzar(r);
+        dado1.mostrar(lblDado1);
     }
 }
