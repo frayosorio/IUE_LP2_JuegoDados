0ddcc6fb31f93e49932771acc12283dcaf1f005e
fray osorio <frayosorio@gmail.com>
2025-08-26 13:07:45 -0500

    Se agregan eventos

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -12,7 +15,7 @@ public class FrmJuego extends JFrame {
     JLabel lblLanzamientos, lblCenas;
 
     public FrmJuego() {
-        setSize(600, 500);
+        setSize(600, 300);
         setTitle("Juego de dados");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(null);
@@ -56,5 +59,40 @@ public class FrmJuego extends JFrame {
         lblCenas.setHorizontalAlignment(SwingConstants.RIGHT);
         lblCenas.setFont(new Font("Tahoma", 1, 72));
         getContentPane().add(lblCenas);
+
+        JButton btnIniciar=new JButton("Iniciar Lanzamientos");
+        btnIniciar.setBounds(10, 20+imgDado.getIconHeight(), 200,25);
+        getContentPane().add(btnIniciar);
+
+        JButton btnLanzar=new JButton("Lanzar");
+        btnLanzar.setBounds(10, 60+imgDado.getIconHeight(), 200,25);
+        getContentPane().add(btnLanzar);
+
+        //agregar eventos
+        btnIniciar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciar();
+            }
+            
+        });
+
+        btnLanzar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lanzar();
+            }
+            
+        });
+    }
+
+    private void iniciar(){
+
+    }
+
+    private void lanzar(){
+
     }
 }
