ba6268f08d36385c909619ff13d34629a7d417b4
fray osorio <frayosorio@gmail.com>
2025-08-26 12:54:42 -0500

    Se agregan lasetiquetas para los contadores

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,9 +1,15 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 public class FrmJuego extends JFrame {
+
+    JLabel lblLanzamientos, lblCenas;
 
     public FrmJuego() {
         setSize(600, 500);
@@ -20,8 +26,35 @@ public class FrmJuego extends JFrame {
 
         JLabel lblDado2 = new JLabel();
         lblDado2.setIcon(imgDado);
-        lblDado2.setBounds(20+imgDado.getIconWidth(), 10, imgDado.getIconWidth(), imgDado.getIconHeight());
+        lblDado2.setBounds(20 + imgDado.getIconWidth(), 10, imgDado.getIconWidth(), imgDado.getIconHeight());
         getContentPane().add(lblDado2);
 
+        JLabel lblTituloLanzamientos = new JLabel("Lanzamientos");
+        lblTituloLanzamientos.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTituloLanzamientos.setBounds(30 + 2 * imgDado.getIconWidth(), 10, 100, 25);
+        getContentPane().add(lblTituloLanzamientos);
+
+        JLabel lblTituloCenas = new JLabel("Cenas");
+        lblTituloCenas.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTituloCenas.setBounds(160 + 2 * imgDado.getIconWidth(), 10, 100, 25);
+        getContentPane().add(lblTituloCenas);
+
+        lblLanzamientos = new JLabel("0");
+        lblLanzamientos.setBounds(30 + 2 * imgDado.getIconWidth(), 40, 100, 100);
+        lblLanzamientos.setBackground(new Color(0, 0, 0));
+        lblLanzamientos.setForeground(new Color(51, 255, 0));
+        lblLanzamientos.setOpaque(true);
+        lblLanzamientos.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblLanzamientos.setFont(new Font("Tahoma", 1, 72));
+        getContentPane().add(lblLanzamientos);
+
+        lblCenas = new JLabel("0");
+        lblCenas.setBounds(160 + 2 * imgDado.getIconWidth(), 40, 100, 100);
+        lblCenas.setBackground(new Color(0, 0, 0));
+        lblCenas.setForeground(new Color(51, 255, 0));
+        lblCenas.setOpaque(true);
+        lblCenas.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblCenas.setFont(new Font("Tahoma", 1, 72));
+        getContentPane().add(lblCenas);
     }
 }
